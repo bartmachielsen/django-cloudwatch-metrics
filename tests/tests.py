aff53d5ae40af63dcc77bba574caebaa693229bd
Bart Machielsen <bartmachielsen@gmail.com>
2023-05-24 14:17:46 +0200

    Initial commit

---
+++ tests/tests.py
@@ -0,0 +1,22 @@
+from django.test import TransactionTestCase
+
+from django_cloudwatch_metrics import metrics
+from django_cloudwatch_metrics.models import MetricAggregation
+
+
+class TestPublishingMetrics(TransactionTestCase):
+    """Test publishing metrics."""
+
+    def setUp(self) -> None:
+        pass
+
+    def test_publishing_metrics(self):
+        """Test publishing metrics."""
+        for _ in range(1000):
+            metrics.increment(
+                metric_name="articles_published",
+                value=1,
+            )
+
+        self.assertTrue(MetricAggregation.objects.filter(metric_name="articles_published").exists())
+        self.assertEqual(MetricAggregation.objects.get(metric_name="articles_published").value, 1000)
